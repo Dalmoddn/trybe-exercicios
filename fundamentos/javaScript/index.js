e7af343771bcd6ee195e1124d7f036ca7886f9cf
Dalmoddn <dalmoddn@gmail.com>
2023-06-01 19:49:48 -0300

    exercicio2

--- fundamentos/javaScript/index.js
+++ fundamentos/javaScript/index.js
@@ -1,8 +1,18 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let contagem = 0;
+let media = 0;
+let mensagem = ''
 for (let index = 0; index < numbers.length; index += 1) {
     const element = numbers[index];
     contagem += element;
 }
-
+media = contagem / numbers.length
+if (media > 20) {
+    mensagem = 'A média aritmética é maior que 20.'
+}   
+else{
+    mensagem = 'O valor da média aritmética é menor que 20.'
+}
 console.log(contagem)
+console.log(media)
+console.log(mensagem)
