599df300d52f49ceb318ee7b99955c8910ec71fe
Dalmoddn <dalmoddn@gmail.com>
2023-06-01 20:43:52 -0300

    exercicio1-2

--- fundamentos/javaScript/index.js
+++ fundamentos/javaScript/index.js
@@ -1,11 +1,19 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let contagem = 0;
 let media = 0;
-let mensagem = ''
+let mensagem = '';
+let comparativo = 0;
+let impares = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     const element = numbers[index];
     contagem += element;
-}
+    if (element > comparativo) {
+        comparativo = element
+    } if (element % 2 !== 0) {
+        impares += 1
+    }    
+    }
+
 media = contagem / numbers.length
 if (media > 20) {
     mensagem = 'A média aritmética é maior que 20.'
@@ -16,3 +24,5 @@ else{
 console.log(contagem)
 console.log(media)
 console.log(mensagem)
+console.log(comparativo)
+console.log(impares)
