f184c1f087e015cc3ba9d399e7bb7c6ce3e8cc28
Dalmoddn <dalmoddn@gmail.com>
2023-06-02 18:04:57 -0300

    sort numbers

--- fundamentos/javaScript/sortNumbers.js
+++ fundamentos/javaScript/sortNumbers.js
@@ -1,10 +1,17 @@
 const sortNumbers = (arrayNumeros) => {
+    let aux;
+
     for (let index = 0; index < arrayNumeros.length; index++) {
         for (let indexDeDentro = index + 1; indexDeDentro < arrayNumeros; indexDeDentro =+ 1) {
-            
+            if (arrayNumeros[index] > arrayNumeros[indexDeDentro]) {
+                aux = arrayNumeros[index];
+                arrayNumeros [index] = arrayNumeros[indexDeDentro];
+                arrayNumeros[indexDeDentro] = aux;
+
+            }
         }
         
     }
-};
+}
 
-sortNumbers([99, 5, 6, 1, 0,]); //[0, 1, 5, 6, 99]
+sortNumbers([99, 5, 6, 1, 0]); //[0, 1, 5, 6, 99]
