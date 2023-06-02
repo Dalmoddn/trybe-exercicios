423e397dd619e2fed8bdc315813aa0b6370c793e
Dalmoddn <dalmoddn@gmail.com>
2023-06-02 17:42:34 -0300

    maior menor

---
+++ fundamentos/javaScript/biggestSmallerNumber.js
@@ -0,0 +1,17 @@
+const biggerSmallerNumber = (arrayNumeros) => {
+    let bigger = arrayNumeros[0]
+    let smaller = arrayNumeros[0]
+
+    for (let index = 0; index < arrayNumeros.length; index += 1) {
+    const element = array[index];
+        if (arrayNumeros[index] > bigger) {
+            bigger = arrayNumeros[index]
+        } else if (arrayNumeros[index] < smaller)
+            smaller = arrayNumeros[index];
+}
+    let mensagem = 'Maior:  ${bigger}'
+                    'Menor: ${smaller}'
+    return mensagem;
+}
+
+biggerSmallerNumber([0, 34, 56, 100, 2]);
