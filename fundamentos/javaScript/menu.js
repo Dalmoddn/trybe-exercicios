bf3213e841f4b6a8a62259f92532689a8b4a63d6
Dalmoddn <dalmoddn@gmail.com>
2023-06-05 17:18:17 -0300

    mcd2

---
+++ fundamentos/javaScript/menu.js
@@ -0,0 +1,18 @@
+const menu = {
+    sandwiches: [
+        {
+            name: 'Big Mac',
+            price: 30.99,
+            ingredientes ['2 burgers', 'alface americana', 'queijo cheddar', 'molho especial'],
+            calories: 500,
+
+        },
+            name: 'Big Tasty',
+            price: 29.99,
+            ingredientes ['burger', 'alface americana', 'queijo cheddar', 'molho especial'],
+            calories: 700,
+    ],
+    dessert: [],
+    drinks: [],
+    sideDishes: [],
+}
