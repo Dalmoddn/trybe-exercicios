59e298405e6b90190b150dabdda36d7301e2beec
Dalmoddn <dalmoddn@gmail.com>
2023-06-21 13:57:03 -0300

    parte1

---
+++ exercicios-como-projeto/sd-34-jogo/script.js
@@ -0,0 +1,5 @@
+let botaoSubmeter = document.querySelector('#survive');
+botaoSubmeter.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.href = './game.html';
+});
